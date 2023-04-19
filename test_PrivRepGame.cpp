802a27a088835836403e93767a5efff4a27d3741
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-04-19 10:55:40 +0200

    implemented ESS check

--- test_PrivRepGame.cpp
+++ test_PrivRepGame.cpp
@@ -225,6 +225,62 @@ void PrintSelectionMutationEquilibriumAllCAllD(const Norm& norm, const Simulatio
   std::cerr << "Elapsed time: " << elapsed.count() << " s\n";
 }
 
+void PrintESSCheck(const Norm& norm, const SimulationParams& params, bool check_local_mutants = false) {
+  auto start = std::chrono::high_resolution_clock::now();
+
+  auto compare_payoffs = [&params](const Norm& resident, const Norm& mutant) -> std::pair<double,double> {
+    PrivateRepGame prg({{resident, params.N-1}, {mutant, 1}}, 123456789);
+    prg.Update(params.n_init, params.q, params.mu_percept, false);
+    prg.ResetCounts();
+    prg.Update(params.n_steps, params.q, params.mu_percept, true);
+    auto c_levels = prg.NormCooperationLevels();
+
+    // pi_res = b[ (N-2)p_{res->res} + p_{mut->res} ] - c[ (N-2)p_{res->res} + p_{res->mut} ]
+    // pi_mut = b[ (N-1)p_{res->mut}                ] - c[ (N-1)p_{mut->res}                ]
+    //
+    // delta_pi = pi_res - pi_mut = b[ (N-2)p_{res->res} + p_{mut->res} - (N-1)p_{res->mut} ] - c[ (N-2)p_{res->res} + p_{res->mut} - (N-1)p_{mut->res} ]
+    //          = b P1 - c P2
+    // if P1 >= 0; b/c > P2/P1
+    // if P1 < 0; b/c < P2/P1
+    double p_res_res = c_levels[0][0];
+    double p_res_mut = c_levels[0][1];
+    double p_mut_res = c_levels[1][0];
+    double P1 = (params.N-2)*p_res_res + p_mut_res - (params.N-1)*p_res_mut;
+    double P2 = (params.N-2)*p_res_res + p_res_mut - (params.N-1)*p_mut_res;
+    if (P1 >= 0.0) {
+      return {P2/P1, std::numeric_limits<double>::infinity()};
+    }
+    else {
+      return {1.0, P2/P1};
+    }
+  };
+
+  std::vector<Norm> mutants = {Norm::AllC(), Norm::AllD()};
+  if (check_local_mutants) {
+    auto locals = LocalMutants(norm);
+    mutants.insert(mutants.end(), locals.begin(), locals.end());
+  }
+
+  std::cerr << "Calculating ESS b/c ranges:" << std::endl;
+  double bc_min = 1.0, bc_max = std::numeric_limits<double>::infinity();
+  for (const auto& mutant : mutants) {
+    auto payoffs = compare_payoffs(norm, mutant);
+    std::cerr << "0x" << std::setfill('0') << std::setw(5) << std::hex << mutant.ID() << std::resetiosflags(std::ios_base::fmtflags(-1))
+              << " " << payoffs.first << ' ' << payoffs.second << std::endl;
+    if (payoffs.first > bc_min) {
+      bc_min = payoffs.first;
+    }
+    if (payoffs.second < bc_max) {
+      bc_max = payoffs.second;
+    }
+  }
+  std::cerr << "ESS b/c range: " << bc_min << " : " << bc_max << std::endl;
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end - start;
+  std::cerr << "Elapsed time: " << elapsed.count() << " s\n";
+}
+
 void PrintCompetition(const Norm& n1, const Norm& n2, const SimulationParams& params) {
   auto start = std::chrono::high_resolution_clock::now();
 
@@ -297,11 +353,11 @@ int main(int argc, char *argv[]) {
   }
 
   if (args.empty()) {
-    // test_RandomNorm();
-    // test_LeadingEight();
-    // test_SelectionMutationEquilibrium();
-    // test_SelectionMutationEquilibrium2();
-    test_SelectionMutationEquilibriumFiniteMu();
+    test_RandomNorm();
+    test_LeadingEight();
+    test_SelectionMutationEquilibrium();
+    test_SelectionMutationEquilibrium2();
+    // test_SelectionMutationEquilibriumFiniteMu();
   }
   else if (args.size() == 1) {
     Norm n = Norm::ParseNormString(args.at(0), swap_gb);
@@ -309,6 +365,7 @@ int main(int argc, char *argv[]) {
     SimulationParams params = j.get<SimulationParams>();
     std::cout << nlohmann::json(params).dump(2) << std::endl;
     PrintSelectionMutationEquilibriumAllCAllD(n, params, check_local_mutants);
+    PrintESSCheck(n, params, check_local_mutants);
   }
   else if (args.size() == 2) {  // if two arguments are given, direct competition between two norms are shown
     Norm n1 = Norm::ParseNormString(args.at(0), swap_gb);
