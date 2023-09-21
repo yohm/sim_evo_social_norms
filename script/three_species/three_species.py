a6fb503169108a8253fa7011e19a6ace09ad6c3b
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-09-21 17:39:30 +0900

    calculate S16 as well

--- script/three_species/three_species.py
+++ script/three_species/three_species.py
@@ -32,8 +32,9 @@ benefit_list = [1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0]
 
 N = 50
 leading_eight = ['L1','L2','L3','L4','L5','L6','L7','L8']
+secondary_sixteen = ['S1','S2','S3','S4','S5','S6','S7','S8','S9','S10','S11','S12','S13','S14','S15','S16']
 updated = False
-for resident in leading_eight:
+for resident in (leading_eight + secondary_sixteen):
   for mu_i,mu in enumerate(mu_list):
     key = (N,mu,resident)
     if key in dat:
@@ -90,4 +91,29 @@ for i, (ax, resident) in enumerate(zip(axs.flat, leading_eight)):
 
 # %%
 fig.savefig('three_species.pdf', bbox_inches='tight', pad_inches=0.3)
+# %%
+
+fig,axs = plt.subplots(4, 4, figsize=(24,24), sharex=True, sharey=True)
+plt.subplots_adjust(wspace=0.02, hspace=0.2)
+custom_x_ticks = [2.0, 3.0, 4.0, 5.0]
+
+for i, (ax, resident) in enumerate(zip(axs.flat, secondary_sixteen)):
+  ax.set_title(resident, fontsize=26)
+  for mu_i,mu in enumerate(mu_list):
+    key = (N,mu,resident)
+    ax.plot(benefit_list, dat[key], label=f'$\mu_a={mu}$', marker='o', color=color_map(mu_i/len(mu_list)))
+    ax.set_xlim([1.2,5.2])
+    ax.set_xticks(custom_x_ticks)
+    ax.set_xticklabels(custom_x_ticks, fontsize=16)
+    ax.set_yticklabels([0.0,0.2,0.4,0.6,0.8,1.0], fontsize=16)
+    ax.set_ylim([0.0,1.0])
+    if i == 3:
+      ax.legend(loc='upper right', fontsize=20)
+    if i >= 12:
+      ax.set_xlabel('benefit', fontsize=24)
+    if i % 4 == 0:
+      ax.set_ylabel('cooperation level', fontsize=24)
+# %%
+fig.savefig('three_species_S16.pdf', bbox_inches='tight', pad_inches=0.3)
+
 # %%
