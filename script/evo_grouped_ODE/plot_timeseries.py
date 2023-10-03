a6aa183d6340a3992f57882a4fbb0d10130eef57
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-10-03 15:06:04 +0900

    plot for grouped evo ODE

---
+++ script/evo_grouped_ODE/plot_timeseries.py
@@ -0,0 +1,16 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+# %%
+# Load data
+dat = np.loadtxt('timeseries.dat')
+
+plt.clf()
+fig, ax = plt.subplots(figsize=(6,4))
+ax.plot(dat[:,0], dat[:,1], label='Cooperation Level')
+ax.set_xlabel('time', fontsize=18)
+ax.set_ylabel('cooperation level', fontsize=18)
+ax.set_ylim(-0.03,1.03)
+
+fig.savefig('timeseries.pdf')
+# %%
