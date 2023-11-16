974bec81d4e215172be0bd1485bd8b9547b78349
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-11-16 16:35:57 +0900

    plot for three-species system

--- script/evo_three_species/three_species_frac_pc.py
+++ script/evo_three_species/three_species_frac_pc.py
@@ -66,9 +66,35 @@ ax.text(0.97, 0.61, 'L7', fontsize=12, color='#222222', horizontalalignment='rig
 ax.text(0.10, 0.09, 'L8', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
 
 # %%
-# sort l8_dat according to the order of the leading eight
-l8_dat
-# %%
-fig.savefig('three_species_frac_pc.pdf', bbox_inches='tight')
+fig.savefig('three_species_frac_pc.pdf', bbox_inches='tight', pad_inches=0.05)
 
 # %%
+# run in advance
+# ../../cmake-build-release/main_well_mixed_evo -3 ../fix_prob_results/second_order_mu0.01/fixation_probs_7.msgpack > frac_pc_second_order.dat
+
+dat2 = np.loadtxt('frac_pc_second_order.dat')
+# %%
+# exclude data points whose dat[0] is in the leading eight
+non_dat2 = dat2[np.isin(dat2[:,0], leading_eight+secondary_sixteen, invert=True)]
+l8_dat2 = dat2[np.isin(dat2[:,0], leading_eight)]
+non_dat2.shape, l8_dat2.shape
+# %%
+plt.clf()
+fig, ax = plt.subplots(figsize=(6, 4), ncols=1)
+ax.plot(non_dat[:,2], non_dat[:,1], '.', color='#6883BA')
+ax.set_xlim(-0.02,1.02)
+ax.set_ylim(-0.02,1.02)
+ax.set_xlabel('self-cooperation level', fontsize=12)
+ax.set_ylabel('equilibrium fraction', fontsize=12)
+
+ax.plot(s16_dat[:,2], s16_dat[:,1], 's', color='#3D3B8E')
+ax.plot(l8_dat[:,2], l8_dat[:,1], 'o', color='#E072A4')
+
+ax.text(0.97, 0.76, 'L1', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
+ax.text(0.86, 0.97, 'L2', fontsize=12, color='#222222', horizontalalignment='left', verticalalignment='top')
+ax.text(0.97, 0.07, 'L3', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
+ax.text(0.97, 0.05, 'L4', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='center')
+ax.text(0.87, 0.15, 'L5', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
+ax.text(0.49, 0.01, 'L6', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
+ax.text(0.97, 0.61, 'L7', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
+ax.text(0.10, 0.09, 'L8', fontsize=12, color='#222222', horizontalalignment='right', verticalalignment='bottom')
